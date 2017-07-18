8049aa024b9c29ac461bc19f338429c262a47964
= <=>
2017-07-17 19:28:26 -0500

    Mon Jul 17 19:28:26 CDT 2017

--- search.py
+++ search.py
@@ -14,7 +14,6 @@ def search(regex):
         if len(g(d, "name")) > 1 and len(g(d, "description")) > 1:
             if bool(r.search(g(d, "access").strip())) or regex == g(d, "access").strip():
                 matches.append(d)
-                if regex is g(d, "access").strip():
-                    matches = [d]
-                    break
+                if regex.strip() == g(d, "access").strip():
+                    return [d]
     return matches
