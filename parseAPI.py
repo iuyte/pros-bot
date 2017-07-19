38f1a70ba441defc6cb6c316f3a5ea49a0a5be59
= <=>
2017-07-18 20:10:46 -0500

    Tue Jul 18 20:10:46 CDT 2017

--- parseAPI.py
+++ parseAPI.py
@@ -56,7 +56,7 @@ def parse(data):
                                 else:
                                         cdef += data[line][3:] + " "
                                 line += 1
-                        cdef = cdef.strip().replace("<code>", "`").replace("</code>", "`").replace("* \\c", "\n   * ")#+u"\u2022 ")
+                        cdef = cdef.strip().replace("<code>", "`").replace("</code>", "`").replace("* \\c", "\n   "+u"\u2022 ")
                         returns = returns.strip()
                         line += 1
                         continue
